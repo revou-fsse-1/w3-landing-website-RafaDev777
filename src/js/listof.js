1682d18de372391ebe2bdc41bb65c5cd998eed47
Rafael Jonathan <rafaeljonathan.work@gmail.com>
2023-02-04 14:12:33 +0700

    update js

--- src/js/listof.js
+++ src/js/listof.js
@@ -1,6 +1,6 @@
 // Header burger button
-const checkbox = document.getElementById("burger-menu");
-const box = document.getElementById("test");
+const checkbox = document.querySelector("#burger-menu");
+const box = document.querySelector("#test");
 
 checkbox.addEventListener("click", function handleClick() {
   if (checkbox.checked) {
@@ -9,3 +9,30 @@ checkbox.addEventListener("click", function handleClick() {
     box.style.display = "none";
   }
 });
+
+// mouse parallax
+(function () {
+  // Add event listener
+  document.addEventListener("mousemove", parallax);
+  const elem = document.querySelector(".mouse-parallax");
+  // Magic happens here
+  function parallax(e) {
+    let _w = window.innerWidth / 2;
+    let _h = window.innerHeight / 2;
+    let _mouseX = e.clientX;
+    let _mouseY = e.clientY;
+    let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${
+      50 - (_mouseY - _h) * 0.01
+    }%`;
+    let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${
+      50 - (_mouseY - _h) * 0.02
+    }%`;
+    let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${
+      50 - (_mouseY - _h) * 0.06
+    }%`;
+    let _depth4 = `${50 - (_mouseX - _w) * 0.1}% ${50 - (_mouseY - _h) * 0.1}%`;
+    let x = `${_depth4},${_depth3}, ${_depth2}, ${_depth1}`;
+    console.log(x);
+    elem.style.backgroundPosition = x;
+  }
+})();
